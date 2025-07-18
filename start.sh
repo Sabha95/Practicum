5a70540ef4c171992ef834d177ce14db08bb3c1f
Bhakti Sushil Khot <155902449+Sabha95@users.noreply.github.com>
2025-07-19 00:07:57 +0100

    Update start.sh
    order change

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Rasa on port $PORT..."
 PORT=${PORT:-5005}
-rasa run --port $PORT --enable-api --cors "*" --host 0.0.0.0 --debug
+rasa run --enable-api --port $PORT --host 0.0.0.0 --cors "*" --debug
